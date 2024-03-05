84bb43c4ec0d49145215a0d29ab683ee45f257b2
David Yin <29613693+E137299@users.noreply.github.com>
2024-03-05 15:47:37 -0600

    initial commit

---
+++ hunt.py
@@ -0,0 +1,49 @@
+# import turtle library
+from turtle import *
+
+############# CLASS AND FUNCTION DEFINITIONS #############
+'''
+Food Class
+'''
+class Food():
+    pass
+
+
+'''
+Predator Class
+'''
+class Predator():
+    pass
+
+'''
+Player Class
+'''
+'''
+Define Board Function
+'''
+def border():
+    pass
+
+
+
+############# PROGRAM #############
+screen = Screen()
+screen.bgcolor("black")
+
+#Generate 10 Food objects
+meals = []
+
+#Generate 5 Predator objects
+
+
+# Game Loop
+while len(meals) > 0:
+    pass
+    # Move all Food objects
+
+    # Move all Predators
+
+screen.mainloop()
+
+
+
